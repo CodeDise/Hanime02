b72b6b8b17d6da846f1daac4ee6e461e240beb9f
CODEDISE <otxstudioofficial@gmail.com>
2025-11-21 13:39:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ OWNER_ID = 6888478102
 MSG_EFFECT = 5046509860389126442
 
 SHORT_URL = "linkshortify.com" # shortner url 
-SHORT_API = "" 
-SHORT_TUT = "https://t.me/How_to_Download_7x/26"
+SHORT_API = "7a541402aeacbdbff361696add89bce2cf8afa4e" 
+SHORT_TUT = "https://t.me/How_to_Download_otx"
 
 # Bot Configuration
 SESSION = "diablo"
@@ -22,9 +22,9 @@ WORKERS = 5
 DB_URI = "mongodb"
 DB_NAME = "yato"
 
-FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [[-1003016571084, True, 10] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = [-1003483464561]   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
